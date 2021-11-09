75837ea2310e87978c28b51f1def5421ca3854ff
andisha <andon.stoichkov@yahoo.com>
2021-11-09 23:57:24 +0200

    Added the whole game function and fixed the variables

--- script.js
+++ script.js
@@ -8,18 +8,41 @@ function computerPlay() {
 // rock beats scissors
 // scissors beat paper
 // if they have duplicate choice they are tied
+let playerWin = 0;
+let computerWin = 0;
 
 function playRound (playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
         return 'You are tied. Play again.';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerWin++;
         return 'You lose! Paper beats rock.';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerWin++;
         return 'You lose! Rock beats scissors.';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerWin++;
         return 'You lose! Scissors beats paper.';
     } else {
+        playerWin++;
         return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}.`;
     }
 }
+
+function game () {
+    const playerSelection = window.prompt('Choose your weapon: rock, paper or scissors', 'rock');
+
+    for (let i = 0; i <5; i++) {
+        playRound(playerSelection, computerPlay());
+    }
+    if (playerWin == computerWin) {
+        console.log(`The game is tied. Score: ${playerWin} : ${computerWin}`);
+    } else if (playerWin > computerWin) {
+        console.log(`You are the winner. Score: ${playerWin} : ${computerWin}`);
+    } else {
+        console.log(`You are the loser. Score: ${playerWin} : ${computerWin}`);
+    }
+}
+game();
