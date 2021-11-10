8e9caa29618f2778a6aa166c556a17954628d92f
andisha <andon.stoichkov@yahoo.com>
2021-11-10 22:47:50 +0200

    Made code in script.js more readable and simple

--- script.js
+++ script.js
@@ -1,48 +1,66 @@
 function computerPlay() {
     const choices = ['rock', 'paper', 'sciossors'];
-    const random = Math.floor(Math.random() * choices.length);
-    return choices[random];
+    const random = Math.random();
+    let randomChoice = random * choices.length;
+    randomChoice = Math.floor(randomChoice);
+    return choices[randomChoice];
 }
 
 // paper beats rock
 // rock beats scissors
 // scissors beat paper
 // if they have duplicate choice they are tied
-let playerWin = 0;
-let computerWin = 0;
+
 
 function playRound (playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase();
+    let result = 'win';
+
     if (playerSelection == computerSelection) {
-        return 'You are tied. Play again.';
+        result = 'tie';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerWin++;
-        return 'You lose! Paper beats rock.';
+        result = 'loss';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerWin++;
-        return 'You lose! Rock beats scissors.';
+        result = 'loss';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerWin++;
-        return 'You lose! Scissors beats paper.';
-    } else {
-        playerWin++;
-        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}.`;
+        result = 'loss'
     }
+
+    return result
 }
 
 function game () {
     const playerSelection = window.prompt('Choose your weapon: rock, paper or scissors', 'rock');
+    const rounds = 5;
+    let playerScore = 0;
+    let computerScore = 0;
 
-    for (let i = 0; i <5; i++) {
-        playRound(playerSelection, computerPlay());
+    for (let i = 0; i < rounds; i++) {
+        let roundResult = playRound(playerSelection, computerPlay());
+
+        if (roundResult == 'win') {
+            playerScore++;
+        } else if (roundResult == 'loss'){
+            computerScore++;
+        }
     }
-    if (playerWin == computerWin) {
-        console.log(`The game is tied. Score: ${playerWin} : ${computerWin}`);
-    } else if (playerWin > computerWin) {
-        console.log(`You are the winner. Score: ${playerWin} : ${computerWin}`);
+
+    let gameResult = '';
+
+    if (playerScore == computerScore) {
+        gameResult = 'The game is tied.'
+    } else if (playerScore > computerScore) {
+        gameResult = 'You are the winner.';
     } else {
-        console.log(`You are the loser. Score: ${playerWin} : ${computerWin}`);
+        gameResult = 'You are the loser.';
     }
+
+    const score = `Score: ${playerScore} : ${computerScore}`;
+
+    const endScreen = gameResult + ' ' + score;
+
+    alert(endScreen);
+
 }
 game();
